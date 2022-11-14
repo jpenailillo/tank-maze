59819a6f456c1f70eca2fcef395dda68eba02214
Fabián Torres <66973871+fatorresc@users.noreply.github.com>
2022-11-14 16:45:12 -0300

    explosion

---
+++ images.g.ts
@@ -0,0 +1,90 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myAnim":
+            case "anim1":return [img`
+. . . . . . . . 
+. . . . . . . . 
+. . 5 5 5 5 . . 
+. . 5 4 4 5 . . 
+. . 5 4 4 5 . . 
+. . 5 5 5 5 . . 
+. . . . . . . . 
+. . . . . . . . 
+`, img`
+. . . . . . . . 
+. . . 5 5 . . . 
+. . 5 6 6 5 . . 
+. 5 6 4 4 6 5 . 
+. 5 6 4 4 6 5 . 
+. . 5 6 6 5 . . 
+. . . 5 5 . . . 
+. . . . . . . . 
+`, img`
+4 . . . . . . 4 
+. 5 . 5 5 . 5 . 
+. . 5 6 6 5 . . 
+. 5 6 4 4 6 5 . 
+. 5 6 4 4 6 5 . 
+. . 5 6 6 5 . . 
+. 5 . 5 5 . 5 . 
+4 . . . . . . 4 
+`, img`
+4 . . 4 4 . . 4 
+. 5 . 5 5 . 5 . 
+. . 5 6 6 5 . . 
+4 5 6 4 4 6 5 4 
+4 5 6 4 4 6 5 4 
+. . 5 6 6 5 . . 
+. 5 . 5 5 . 5 . 
+4 . . 4 4 . . 4 
+`, img`
+b . . a a . . b 
+. b . a a . b . 
+. . b a a b . . 
+a a a 4 4 a a a 
+a a a 4 4 a a a 
+. . b a a b . . 
+. b . a a . b . 
+b . . a a . . b 
+`, img`
+. . . . . . . . 
+. b . b b . b . 
+. . b a a b . . 
+. b a c c a b . 
+. b a c c a b . 
+. . b a a b . . 
+. b . b b . b . 
+. . . . . . . . 
+`, img`
+. . . . . . . . 
+. . . . . . . . 
+. . . d d . . . 
+. . d b b d . . 
+. . d b b d . . 
+. . . d d . . . 
+. . . . . . . . 
+. . . . . . . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ tilemap.g.ts
@@ -0,0 +1,39 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tileGrass2], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
