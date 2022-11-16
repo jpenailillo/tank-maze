14890e31ddfcc7bc152aa92dfb1976dc9f1f9daf
Julio Peñailillo <65561498+jpenailillo@users.noreply.github.com>
2022-11-16 11:17:38 -0300

    update

--- images.g.ts
+++ images.g.ts
@@ -1,154 +1,16 @@
-// Código generado automáticamente. No editar.
+// Auto-generated code. Do not edit.
 namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "image1":
-            case "tank90":return img`
-f f f f f f f f f f f . 
-f c f c f c f c f c f . 
-f c f c f c f c f c f . 
-. . 3 2 3 2 2 3 . . . . 
-. 3 2 2 c f c 3 3 . f c 
-. 1 2 2 c 4 6 c c c f c 
-. 1 3 3 c 5 5 c f f f f 
-. 3 3 2 f f f 3 3 . f f 
-. . 2 3 3 2 3 3 . . . . 
-f c f c f c f c f c f . 
-f c f c f c f c f c f . 
-f f f f f f f f f f f . 
-`;
-            case "image2":
-            case "tank180":return img`
-f f f . . . . . . f f f 
-f c c . 3 1 1 3 . c c f 
-f f f 2 3 3 2 2 3 f f f 
-f c c 3 2 3 2 2 2 c c f 
-f f f 3 f c c c 3 f f f 
-f c c 2 f 5 4 f 2 c c f 
-f f f 3 f 5 6 c 2 f f f 
-f c c 3 3 c c 3 3 c c f 
-f f f . 3 f c 3 . f f f 
-f c c . . f c . . c c f 
-f f f . f f f f . f f f 
-. . . . f f c c . . . . 
-`;
-            case "X9Pb{~pS%c*MdKq-UUI5":
-            case "tank":return img`
-. . . . c c f f . . . . 
-f f f . f f f f . f f f 
-f c c . . c f . . c c f 
-f f f . 3 c f 3 . f f f 
-f c c 3 3 c c 3 3 c c f 
-f f f 2 c 6 5 f 3 f f f 
-f c c 2 f 4 5 f 2 c c f 
-f f f 3 c c c f 3 f f f 
-f c c 2 2 2 3 2 3 c c f 
-f f f 3 2 2 3 3 2 f f f 
-f c c . 3 1 1 3 . c c f 
-f f f . . . . . . f f f 
-`;
-            case "image3":
-            case "myImage":return img`
-. . . . . . . . . . . . 
-. . . . . . . . . . . . 
-. . . . . . . . . . . . 
-. . . . . . . . . . . . 
-. . . . . . . . . . . . 
-. . . . . . . . . . . . 
-. . . . . . . . . . . . 
-. . . . . . . . . . . . 
-. . . . . . . . . . . . 
-. . . . . . . . . . . . 
-. . . . . . . . . . . . 
-. . . . . . . . . . . . 
-`;
-            case "image4":
-            case "tank270":return img`
-. f f f f f f f f f f f 
-. f c f c f c f c f c f 
-. f c f c f c f c f c f 
-. . . . 3 3 2 3 3 2 . . 
-f f . 3 3 f f f 2 3 3 . 
-f f f f c 5 5 c 3 3 1 . 
-c f c c c 6 4 c 2 2 1 . 
-c f . 3 3 c f c 2 2 3 . 
-. . . . 3 2 2 3 2 3 . . 
-. f c f c f c f c f c f 
-. f c f c f c f c f c f 
-. f f f f f f f f f f f 
-`;
+
         }
         return null;
     })
 
     helpers._registerFactory("animation", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "myAnim":
-            case "anim1":return [img`
-. . . . . . . . 
-. . . . . . . . 
-. . 5 5 5 5 . . 
-. . 5 4 4 5 . . 
-. . 5 4 4 5 . . 
-. . 5 5 5 5 . . 
-. . . . . . . . 
-. . . . . . . . 
-`, img`
-. . . . . . . . 
-. . . 5 5 . . . 
-. . 5 6 6 5 . . 
-. 5 6 4 4 6 5 . 
-. 5 6 4 4 6 5 . 
-. . 5 6 6 5 . . 
-. . . 5 5 . . . 
-. . . . . . . . 
-`, img`
-4 . . . . . . 4 
-. 5 . 5 5 . 5 . 
-. . 5 6 6 5 . . 
-. 5 6 4 4 6 5 . 
-. 5 6 4 4 6 5 . 
-. . 5 6 6 5 . . 
-. 5 . 5 5 . 5 . 
-4 . . . . . . 4 
-`, img`
-4 . . 4 4 . . 4 
-. 5 . 5 5 . 5 . 
-. . 5 6 6 5 . . 
-4 5 6 4 4 6 5 4 
-4 5 6 4 4 6 5 4 
-. . 5 6 6 5 . . 
-. 5 . 5 5 . 5 . 
-4 . . 4 4 . . 4 
-`, img`
-b . . a a . . b 
-. b . a a . b . 
-. . b a a b . . 
-a a a 4 4 a a a 
-a a a 4 4 a a a 
-. . b a a b . . 
-. b . a a . b . 
-b . . a a . . b 
-`, img`
-. . . . . . . . 
-. b . b b . b . 
-. . b a a b . . 
-. b a c c a b . 
-. b a c c a b . 
-. . b a a b . . 
-. b . b b . b . 
-. . . . . . . . 
-`, img`
-. . . . . . . . 
-. . . . . . . . 
-. . . d d . . . 
-. . d b b d . . 
-. . d b b d . . 
-. . . d d . . . 
-. . . . . . . . 
-. . . . . . . . 
-`];
+
         }
         return null;
     })
@@ -161,4 +23,4 @@ b . . a a . . b
     })
 
 }
-// Código generado automáticamente. No editar.
+// Auto-generated code. Do not edit.
