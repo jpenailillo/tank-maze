b2dfd7895e89ad66a2d1493c5e92c89aa63c3c5d
Julio Peñailillo <65561498+jpenailillo@users.noreply.github.com>
2022-11-16 14:26:21 -0300

    aa

--- panel.ts
+++ panel.ts
@@ -5,17 +5,28 @@ interface IPanelConfig {
 
 class Panel {
 	config: IPanelConfig;
+    battery: Image;
 
 	constructor(config: IPanelConfig) {
 		this.config = config;
 		this.draw();
+        this.drawEnergy(5);
 	}
 
 	draw() {
-		const background = image.create(40, 220);
-		background.fill(13);
-		sprites.create(background).setPosition(this.config.x, this.config.y);
+		// const background = image.create(14, 220);
+		// background.fill(4);
+		// sprites.create(background).setPosition(6 + this.config.x, 8 + this.config.y);
+        this.battery = image.create(7, 54);
+        sprites.create(this.battery).setPosition(6, 44);
 	}
 
+    drawEnergy(count: number){
+        // this.battery.fill(10)
+        for(let i = 0; i < 14; i++) {
+            // this.battery.fill(2)
+            this.battery.fillRect(0, 4*i, 7, 2, 11);
+        }
+    }
 
 }
