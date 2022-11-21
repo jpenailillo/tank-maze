15316174c3d83fbfd1b0beee62e8e77aeab8f616
Juliuz <jpenailillo@outlier.cl>
2022-11-21 11:56:26 -0300

    board

--- init.ts
+++ init.ts
@@ -1,4 +1,5 @@
-let board = new Board(13, 4);
-let tank = new Tank(board, board.getCell(0, 1), Direction.Right);
-let panel = new Panel();
+const level = levels[0];
+const board = new Board(level, 13, 4);
+const tank = new Tank(board, board.getCell(0, 1), Direction.Right);
+const panel = new Panel();
 scene.setBackgroundColor(15);
